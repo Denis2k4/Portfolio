4a3836219e40b707bb46829bc7f24af05529e1bc
zug <164471743+Zugpro2k4@users.noreply.github.com>
2025-08-20 16:21:18 +0700

    update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export const Home = () => {
+  return ( 
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+
+      {/* Theme Toggle */}
+        hi
+      {/* Background Effect */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+
+    </div>
+  );
+};
